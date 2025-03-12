8c4944194d2691d1f7e4a6aee64219dde5f13a19
ZeroDrago24 <zerodragon.stream@gmail.com>
2025-03-12 11:53:00 -0400

    Desarrollador 3: Corrección de error en Saludo.java y agregado un nuevo mensaje

--- Saludo.java
+++ Saludo.java
@@ -1,5 +1,8 @@
 public class Saludo { 
 	public static void main(String[] args) { 
-		System.out.println(¡Hola, equipo!);} 
+		System.out.println("¡Hola, equipo!");
+		System.out.println("Mucho gusto :) soy Zero";
+	} 
 } 
+
 
